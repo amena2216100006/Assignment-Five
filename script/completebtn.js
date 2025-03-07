6160b968658e47fc97c2bd0761bf5a01a21fa28e
amena2216100006 <amenaakter2216100006@gmail.com>
2025-03-07 12:48:30 +0600

    clear history and last alert done

--- script/completebtn.js
+++ script/completebtn.js
@@ -1,4 +1,12 @@
-document.getElementById("complete-btn").addEventListener("click", function(){
+ 
+//  current day
+// document.getElementById("currentDay").innerText = `${new Date().getDay()}`;
+//  current date
+document.getElementById("currentDate").innerText = `${new Date().toDateString()}`;
+ 
+
+
+document.getElementById("complete-btn").addEventListener("click", function btn1(){
     alert("Board updated Successfully");
     let taskNum = document.getElementById("task-num").innerText;
     let convertedTaskNum = parseInt(taskNum);
@@ -22,7 +30,7 @@ document.getElementById("complete-btn").addEventListener("click", function(){
 
 // 2nd card
 
-document.getElementById("complete-btn-2").addEventListener("click", function(){
+document.getElementById("complete-btn-2").addEventListener("click", function btn2(){
     alert("Board updated Successfully");
     let taskNum = document.getElementById("task-num").innerText;
     let convertedTaskNum = parseInt(taskNum);
@@ -47,7 +55,7 @@ document.getElementById("complete-btn-2").addEventListener("click", function(){
 //  3rd card 
 
 
-document.getElementById("complete-btn-3").addEventListener("click", function(){
+document.getElementById("complete-btn-3").addEventListener("click", function btn3(){
     alert("Board updated Successfully");
     let taskNum = document.getElementById("task-num").innerText;
     let convertedTaskNum = parseInt(taskNum);
@@ -74,7 +82,7 @@ document.getElementById("complete-btn-3").addEventListener("click", function(){
 
 
 
-document.getElementById("complete-btn-4").addEventListener("click", function(){
+document.getElementById("complete-btn-4").addEventListener("click", function btn4(){
     alert("Board updated Successfully");
     let taskNum = document.getElementById("task-num").innerText;
     let convertedTaskNum = parseInt(taskNum);
@@ -102,7 +110,7 @@ document.getElementById("complete-btn-4").addEventListener("click", function(){
 
 
 
-document.getElementById("complete-btn-5").addEventListener("click", function(){
+document.getElementById("complete-btn-5").addEventListener("click", function btn5(){
     alert("Board updated Successfully");
     let taskNum = document.getElementById("task-num").innerText;
     let convertedTaskNum = parseInt(taskNum);
@@ -129,7 +137,7 @@ document.getElementById("complete-btn-5").addEventListener("click", function(){
 
 
 
-document.getElementById("complete-btn-6").addEventListener("click", function(){
+document.getElementById("complete-btn-6").addEventListener("click", function btn6(){
     alert("Board updated Successfully");
     let taskNum = document.getElementById("task-num").innerText;
     let convertedTaskNum = parseInt(taskNum);
@@ -149,3 +157,36 @@ document.getElementById("complete-btn-6").addEventListener("click", function(){
     history.appendChild(newDiv);
     document.getElementById("complete-btn-6").disabled = true;
 })
+
+// clear history
+
+document.getElementById("clear-btn").addEventListener("click", function(){
+    document.getElementById("history-div").textContent = "";
+})
+ 
+// last alert;
+// function lastAlert (){
+//     btn1()
+//     btn2()
+//     btn3()
+//     btn4()
+//     btn5()
+//     btn6()
+//     alert("congrates!!!  You have completed all the current task");
+// }
+
+// last alert
+let buttons = document.querySelectorAll(".completed-btn");
+let totalButtons = buttons.length;
+let clickedCount = 0;
+for(let i= 0; i< totalButtons; i++){
+    buttons[i].addEventListener("click",function(){
+        if(!buttons[i].classList.contains("clicked")){
+            buttons[i].classList.add("clicked");
+            clickedCount++;
+        }
+        if(clickedCount === totalButtons){
+            alert("congrates!!!  You have completed all the current task");
+        }
+    })
+}
